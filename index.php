b805ad085e0c8ac44d6a4e16f307b830fa81f782
Pham Van Manh <manh.pham21@pn.com.vn>
2020-06-12 15:32:08 +0700

    update index.php file the first

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-abc
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>hello PHP</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	echo N;
+</body>
+</html>
